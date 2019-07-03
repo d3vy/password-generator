f00502a0d642aff20ef2baded15136dbf36efe31
Szabolcs Surányi <szabolcs.suranyi@shadesoft.hu>
2019-07-03 12:03:23 +0200

    Update methods

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
 require_once 'vendor/autoload.php';
 
 use Symfony\Component\Security\Core\Encoder\{
-    BCryptPasswordEncoder, Pbkdf2PasswordEncoder
+    Argon2iPasswordEncoder, BCryptPasswordEncoder, Pbkdf2PasswordEncoder, SodiumPasswordEncoder
 };
+use Symfony\Component\Security\Core\Exception\LogicException;
 
 const DEFAULT_LENGTH    = 16;
 const DEFAULT_COST      = 13;
@@ -13,6 +14,18 @@ $length     = $_GET['length'] ?? DEFAULT_LENGTH;
 $encodeCost = $_GET['cost'] ?? DEFAULT_COST;
 
 $plainPassword = substr(rtrim(strtr(base64_encode(random_bytes($length * 2)), '+/', '-_'), '='), 0, $length);
+
+try {
+    $argon2iPassword = (new Argon2iPasswordEncoder)->encodePassword($plainPassword, null);
+} catch(\LogicException $ex) {
+    $argon2iPassword = '(not supported)';
+}
+
+try {
+    $sodiumPassword = (new SodiumPasswordEncoder)->encodePassword($plainPassword, null);
+} catch(LogicException $ex) {
+    $sodiumPassword = '(not supported)';
+}
 
 echo (new Pug)->render('output.pug', [
     'defaults'          => [
@@ -23,5 +36,7 @@ echo (new Pug)->render('output.pug', [
     'md5Password'       => md5($plainPassword),
     'sha1Password'      => sha1($plainPassword),
     'bcryptPassword'    => (new BCryptPasswordEncoder($encodeCost))->encodePassword($plainPassword, null),
-    'pbkdf2Password'    => (new Pbkdf2PasswordEncoder)->encodePassword($plainPassword, null)
+    'pbkdf2Password'    => (new Pbkdf2PasswordEncoder)->encodePassword($plainPassword, null),
+    'argon2iPassword'   => $argon2iPassword,
+    'sodiumPassword'    => $sodiumPassword
 ]);
