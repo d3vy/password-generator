e3850a8ac7460cde7b08a8e4559515a675b0ae95
Szabolcs Surányi <arnyek@arnyek.hu>
2022-12-14 13:59:05 +0100

    update generation and info

--- index.php
+++ index.php
@@ -1,23 +1,21 @@
 <?php
 
-require_once 'vendor/autoload.php';
-
-use Symfony\Component\Security\Core\Encoder\{
-    Argon2iPasswordEncoder, BCryptPasswordEncoder, Pbkdf2PasswordEncoder, SodiumPasswordEncoder
-};
+use Symfony\Component\Security\Core\Encoder\{Argon2iPasswordEncoder, Pbkdf2PasswordEncoder, SodiumPasswordEncoder};
 use Symfony\Component\Security\Core\Exception\LogicException;
 
-const DEFAULT_LENGTH    = 16;
-const DEFAULT_COST      = 13;
+require_once 'vendor/autoload.php';
 
-$length     = $_GET['length'] ?? DEFAULT_LENGTH;
+const DEFAULT_LENGTH = 16;
+const DEFAULT_COST = 13;
+
+$length = $_GET['length'] ?? DEFAULT_LENGTH;
 $encodeCost = $_GET['cost'] ?? DEFAULT_COST;
 
 $plainPassword = substr(rtrim(strtr(base64_encode(random_bytes($length * 2)), '+/', '-_'), '='), 0, $length);
 
 try {
     $argon2iPassword = (new Argon2iPasswordEncoder)->encodePassword($plainPassword, null);
-} catch(\LogicException $ex) {
+} catch(LogicException $ex) {
     $argon2iPassword = '(not supported)';
 }
 
@@ -27,16 +25,16 @@ try {
     $sodiumPassword = '(not supported)';
 }
 
-echo (new Pug)->render('output.pug', [
-    'defaults'          => [
-        'length'    => DEFAULT_LENGTH,
-        'cost'      => DEFAULT_COST
+echo (new Pug(['cache' => './cache/']))->render('output.pug', [
+    'defaults' => [
+        'length' => DEFAULT_LENGTH,
+        'cost' => DEFAULT_COST
     ],
-    'plainPassword'     => $plainPassword,
-    'md5Password'       => md5($plainPassword),
-    'sha1Password'      => sha1($plainPassword),
-    'bcryptPassword'    => (new BCryptPasswordEncoder($encodeCost))->encodePassword($plainPassword, null),
-    'pbkdf2Password'    => (new Pbkdf2PasswordEncoder)->encodePassword($plainPassword, null),
-    'argon2iPassword'   => $argon2iPassword,
-    'sodiumPassword'    => $sodiumPassword
+    'plainPassword' => $plainPassword,
+    'md5Password' => md5($plainPassword),
+    'sha1Password' => sha1($plainPassword),
+    'bcryptPassword' => password_hash($plainPassword, PASSWORD_BCRYPT, ['cost' => $encodeCost]),
+    'pbkdf2Password' => (new Pbkdf2PasswordEncoder)->encodePassword($plainPassword, null),
+    'argon2iPassword' => $argon2iPassword,
+    'sodiumPassword' => $sodiumPassword
 ]);
